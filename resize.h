f4f46ca77959128f4a4ce902c4c2922b3beac114
Dias <dias_bolatov10@mail.com>
2020-11-22 21:41:20 +0500

    HW6

---
+++ resize.h
@@ -0,0 +1,6 @@
+#ifndef RESIZE_H
+#define RESIZE_H
+
+char* resize(char* str, unsigned size, unsigned new_size);
+
+#endif  // RESIZE_H
---
+++ strcat1.h
@@ -0,0 +1,6 @@
+#ifndef STRCAT1_H
+#define STRCAT1_H
+
+void strcat1(char* to, const char* from);
+
+#endif  // STRCAT1_H
