a3b096db90410036d88b4a2489bdc36ee55711b3
ZeByR0 <mezin.2001@gmail.com>
2020-11-25 17:25:55 +0300

    HW by Mezin Igor

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-
+#include "task1.h"
+#include "task2.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  // task1
+  char str[] = "qwerty";
+  char str2[] = "1234";
+  task1::strcat(str, str2);
+  std::cout << str << endl;
+
+  // task2
+  char* a = new char[12]{ '1', '2', '3', '4', '5', '6' };
+  char* b = task2::resize(a, 12, 24);
+  b[23] = '2';
+  std::cout << b[23] << endl;
+  delete[] b;
+  return 0;
 }
---
+++ task1.h
@@ -0,0 +1,12 @@
+#ifndef TASK1_H
+#define TASK1_H
+#include <cstring>
+class task1
+{
+public:
+  task1();
+
+  static void strcat(char* to, const char* from);
+};
+
+#endif  // TASK1_H
