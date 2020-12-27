46e0f1502374f13b3030c271a6cf72e5b9f38312
Sergei <kahu908@gmail.com>
2020-12-27 22:40:50 +0300

    HW6 Makarov Sergey

--- main.cpp
+++ main.cpp
@@ -2,8 +2,41 @@
 
 using namespace std;
 
+void strcat(char* to, const char* from)
+{
+    while (*to)
+        to++;
+    while (*from)
+    {
+        *to = *from;
+        to++;
+        from++;
+    }
+    *to = '\0';
+}
+
+char* resize(char* str, unsigned size, unsigned new_size)
+{
+    char *pointer;
+    unsigned i = 0;
+
+    if (new_size < size)
+        return (0);
+    pointer = new char[new_size];
+    while (i < size)
+    {
+        pointer[i] = str[i];
+        i++;
+    }
+    delete[] str;
+    return (pointer);
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    char to[20] = "Hello \0";
+    char from[] = "world!";
+    strcat(to, from);
+    printf("%s\n", to);
     return 0;
 }
