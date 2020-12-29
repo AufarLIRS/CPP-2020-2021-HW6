300203ed021834a66608c031e8776cd34dd117ce
APGDiamond <shalmaz1733@gmail.com>
2020-12-29 22:11:00 +0300

    HW6

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include "strcat.cpp"
+#include "resize.cpp"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    char a[] = "Hello";
+    char b[] = " it's me, Mario!";
+    strcat(a, b);
+    std::cout << a << std::endl;
+    unsigned int size = 2;
+    char* s1 = new char[size];
+    *s1 = 's';
+    std::cout << *s1 << std::endl;
+    s1 = resize(s1, size, 4);
+    std::cout << *s1 << std::endl;
+    char* s2 = new char[4];
+    std::cout << *s2 << std::endl;
+    s2 = resize(s2, 4, 2);
+    std::cout << *s2 << std::endl;
+    delete[] s1;
+    delete[] s2;
+    return 1;
 }
---
+++ strcat.cpp
@@ -0,0 +1,11 @@
+#include "strcat.h"
+
+void strcat(char* to, const char* from)
+{
+    while (*to != 0)
+        ++to;
+    do
+    {
+        *to = *from;
+    } while (++to, *from++ != 0);
+}
